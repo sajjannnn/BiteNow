a4b1e85e09060e9fe8888eb3a8e3c4528468d500
Sajjan Kumar <sajjanaasht@gmail.com>
2025-11-19 02:25:31 +0530

    added cors proxy

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -11,7 +11,7 @@ const Body = () => {
 
      useEffect(() => {
      const fetchData = async() => {
-      const data= await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+      const data= await fetch("https://corsproxy.io/https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
       const json = await data.json();
       console.log(json);
       
