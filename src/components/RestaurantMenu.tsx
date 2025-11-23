c4ed175316782530f6088e5d5c76939750fd1913
Sajjan Kumar <sajjanaasht@gmail.com>
2025-11-23 23:54:29 +0530

    updated url

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -6,8 +6,9 @@ const RestaurantMenu = () => {
     useEffect(() => {
         // fetch restaurant menu data
         const fetchMenu = async () => {
-            const res = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.63270&lng=77.21980&restaurantId=752411&catalog_qa=undefined&submitAction=ENTER");
-            const data = await res.json();
+           
+            const response = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.63270&lng=77.21980&restaurantId=752411");
+            const data = await response.text();
             console.log(data);
             
         };
